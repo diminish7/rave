edff02458044a3972200e07128101a6c7ddfda1a
unknown <Jason Rush@.(none)>
2009-06-19 19:24:03 -0700

    cleanup

--- examples/appropriate-casey/tmp/war/WEB-INF/gems/gems/rave-0.1.0/examples/casey/config.ru
+++
@@ -1,2 +0,0 @@
-require 'robot'
-run Casey::Robot.new( :name => "casey" )
--- examples/appropriate-casey/tmp/war/WEB-INF/gems/gems/rave-0.1.0/examples/casey/config/warble.rb
+++
@@ -1,4 +0,0 @@
-Warbler::Config.new do |config|
-  config.gems = %w( rave rbconfig )
-  config.includes = %w( robot.rb appengine-web.xml )
-end
