8b7c2f9e01f56ece991db37ac26cd6085582878f
Bil Bas <bil.bagpuss@gmail.com>
2009-12-02 14:20:47 +0000

    ADDED: all rspecs for Wavelet (including those that I didn't create when extending that class; sorry).

---
+++ test/test_wavelet.rb
@@ -0,0 +1,81 @@
+require File.join(File.dirname(__FILE__), "helper")
+
+describe Rave::Models::Wavelet do
+  before do
+    # A wavelet must contain at least one blip, so create this minimum system.
+    @root_blip = Blip.new(:id => "b+blip", :wavelet_id => "w+wavelet", :wave_id => "w+wave")
+    @wavelet = Wavelet.new(:id => "w+wavelet", :wave_id => "w+wave", :root_blip_id => "b+blip")
+    @wave = Wave.new(:id => "w+wave")
+    @context = Context.new(:wavelets => { "w+wavelet" => @wavelet },
+      :waves => { "w+wave" => @wave },
+      :blips => { "b+blip" => @root_blip })
+  end
+
+  describe "final_blip()" do
+    it "should be the root blip if this is the only one" do
+      @wavelet.final_blip.should == @root_blip
+    end
+
+    it "should be the last one in the thread of 2" do
+      new_blip = @wavelet.create_blip
+      @wavelet.final_blip.should == new_blip
+    end
+
+    it "should be the last one in the thread of 4" do
+      @wavelet.create_blip
+      @wavelet.create_blip
+      new_blip = @wavelet.create_blip
+      @wavelet.final_blip.should == new_blip
+    end
+  end
+
+  describe "root_blip()" do
+    it "should be the initial root blip, regardless of number of replies" do
+      @wavelet.root_blip.should == @root_blip
+      @wavelet.create_blip
+      @wavelet.root_blip.should == @root_blip
+    end
+  end
+
+  describe "wave()" do
+    it "should return the wave that the wavelet is in" do
+      @wavelet.wave.should == @wave
+    end
+  end
+
+  describe "operations" do
+    describe "create_blip()" do
+      it "should create a blip at the end of the thread and an operation to the context" do
+        new_blip = @wavelet.create_blip
+        new_blip.parent_blip.should == @root_blip
+        new_blip.wave.should == @wave
+        new_blip.wavelet.should == @wavelet
+        new_blip.child_blips.should == []
+        @root_blip.child_blips.should == [new_blip]
+        @context.blips[new_blip.id].should == new_blip
+        validate_operations(@context, [Operation::WAVELET_APPEND_BLIP])
+      end
+
+      it "should work correctly multiple times to create a thread" do
+        new_blip1 = @wavelet.create_blip
+        new_blip2 = @wavelet.create_blip
+        @root_blip.child_blips.should == [new_blip1] # Not changed.
+        new_blip1.child_blips.should == [new_blip2]
+        new_blip2.child_blips.should == []
+        new_blip2.parent_blip.should == new_blip1
+        @context.blips[new_blip2.id].should == new_blip2
+        validate_operations(@context, [Operation::WAVELET_APPEND_BLIP, Operation::WAVELET_APPEND_BLIP])
+      end
+    end
+    
+    describe "add_participant()" do
+      it "should add a participant to the wavelet and an operation to the context" do
+        @wavelet.participants.should == Set.new
+        @wavelet.add_participant("Dave")
+        @wavelet.participants.should == Set.new(["Dave"])
+        validate_operations(@context, [Operation::WAVELET_ADD_PARTICIPANT])
+      end
+    end
+  end
+
+end
