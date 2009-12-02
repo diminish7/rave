45e55cb6c31456adffb198c0ec5c522c64bd4490
Bil Bas <bil.bagpuss@gmail.com>
2009-12-02 14:42:43 +0000

    ADDED: some rspecs for Event

--- test/test_event.rb
+++ test/test_event.rb
@@ -17,5 +17,31 @@ describe Rave::Models::Event do
       Rave::Models::Event.valid_event_type?("INVALID_EVENT").should be_false
     end
   end
+
+  describe "create()" do
+     it "should return the appropriate event sub-class for all valid events" do
+      Rave::Models::Event::EVENT_CLASSES.each do |event|
+        Rave::Models::Event.create(:type => event.type).should be_a_kind_of event
+      end
+    end
+
+    it "should raise an exception for an invalid event" do
+      lambda { Rave::Models::Event.create(:type => "INVALID_EVENT") }.should raise_error Exception
+    end
+
+    it "should raise an exception without a type specified" do
+      lambda { Rave::Models::Event.create }.should raise_error Exception
+    end
+  end
+
+  describe "type()" do
+    it "should raise an exception when called on Event" do
+      lambda { Rave::Models::Event.type }.should raise_error Exception
+    end
+
+    it "should return the correct type for a BlipSubmittedEvent" do
+      Rave::Models::Event::BlipSubmittedEvent.type.should == 'BLIP_SUBMITTED'
+    end
+  end
   
 end
