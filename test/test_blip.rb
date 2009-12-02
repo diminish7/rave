054e82c9d75d7c507f390ebd7bea7d68fd50a0a1
Bil Bas <bil.bagpuss@gmail.com>
2009-12-02 13:29:31 +0000

    ADDED: rspecs for Blip (that I didn't create when extending that class; sorry).

--- test/test_blip.rb
+++ test/test_blip.rb
@@ -27,6 +27,60 @@ describe Rave::Models::Blip do
       blip.has_annotation?("test").should be_true
     end
   end
+
+  describe "add_child_blip()" do
+    it "should add the blip to its children and to context" do
+      parent = Blip.new(:id => "b+parent")
+      context = Context.new(:blips => { "b+parent" => parent })
+      child = Blip.new(:id => "TBD+b+child")
+      parent.child_blips.should == []
+      context.blips.should == { "b+parent" => parent }
+
+      parent.add_child_blip(child)
+      parent.child_blips.should == [child]
+      context.blips.should == { "b+parent" => parent, "TBD+b+child" => child }
+    end
+  end
+
+  describe "child_blips()" do
+    it "should list the blips that are children of the blip" do
+      parent = Blip.new(:id => "b+parent")
+      child = Blip.new(:id => "b+child")
+      context = Context.new(:blips => { "b+parent" => parent, "b+child" => child })
+      parent.child_blips.should == []
+
+      parent.add_child_blip(child)
+      parent.child_blips.should == [child]
+    end
+  end
+
+  describe "parent_blip()" do
+    it "should return the parent of the blip, if any" do
+      parent = Blip.new(:id => "b+parent")
+      child = Blip.new(:id => "b+child", :parent_blip_id => "b+parent")
+      context = Context.new(:blips => { "b+parent" => parent, "b+child" => child })
+      parent.parent_blip.should be_nil
+      child.parent_blip.should == parent
+    end
+  end
+
+  describe "wavelet()" do
+    it "should return the blip's wavelet" do
+      blip = Blip.new(:id => "b+blip", :wavelet_id => "w+wavelet")
+      wavelet = Wavelet.new(:id => "w+wavelet")
+      context = Context.new(:blips => { "b+parent" => blip }, :wavelets => { "w+wavelet" => wavelet })
+      blip.wavelet.should == wavelet
+    end
+  end
+
+  describe "wave()" do
+    it "should return the blip's wavelet" do
+      blip = Blip.new(:id => "b+blip", :wave_id => "w+wave")
+      wave = Wave.new(:id => "w+wave")
+      context = Context.new(:blips => { "b+parent" => blip }, :waves => { "w+wave" => wave })
+      blip.wave.should == wave
+    end
+  end
   
   describe "operations" do
   
@@ -126,7 +180,39 @@ describe Rave::Models::Blip do
         validate_operations(blip.context, [Operation::DOCUMENT_APPEND])
       end
     end
-    
+
+    describe "create_child_blip()" do
+      before do
+        @parent = Blip.new(:id => "b+parent", :wavelet_id => "w+wavelet",
+          :wave_id => "w+wave")
+        @context = Context.new(
+          :blips => { "b+parent" => @parent },
+          :wavelets => { "w+wavelet" => Wavelet.new(:id => "w+wavelet") },
+          :waves => { "w+wave" => Wave.new(:id => "w+wave") }
+        )
+        @child = @parent.create_child_blip
+      end
+
+      it "should create a new, empty blip as a child of the parent" do
+        @child.content.should == ''
+        @child.should_not == @parent
+        @parent.child_blips.should == [@child]
+        @child.parent_blip.should == @parent
+        @context.blips.should == { "b+parent" => @parent, @child.id => @child }
+        @child.wave.should == @parent.wave
+        @child.wavelet.should == @parent.wavelet
+      end
+
+      it "should create blips with unique ids starting with TBD" do
+        @child.id.should =~ /^TBD./
+        child2 = @parent.create_child_blip
+        child2.id.should =~ /^TBD./
+        @child.id.should_not == child2.id
+      end
+
+      it "should add an insert operation to the context" do
+        validate_operations(@context, [Operation::BLIP_CREATE_CHILD])
+      end
+    end
   end
-  
 end
