e5e823da022565404b973fd9957607643f552176
Bil Bas <bil.bagpuss@gmail.com>
2009-12-07 18:56:50 +0000

    FIXED: missed a Casey compile error.

--- examples/appropriate-casey/robot.rb
+++ examples/appropriate-casey/robot.rb
@@ -6,24 +6,23 @@ module MyRaveRobot
    
     #This is a very simple robot that tries to tone down yelling in waves
     def document_changed(event, context)
-      unless [id, "spelly@gwave.com"].include event.modified_by_id
-          new_sentence = true
-          event.blip.content.length.times do |index|
-            range = index..index+1
-            char = blip.content[index, 1]
-            if char =~ /[A-Z]/ && !new_sentence
-              blip.set_text_in_range(range, char.downcase)
-            elsif char =~ /[a-z]/ && new_sentence
-              blip.set_text_in_range(range, char.upcase)
-            elsif char == "!"
-              if new_sentence
-                blip.delete_range(range)
-              else
-                blip.set_text_in_range(range, ".")
-              end
+      unless [id, "spelly@gwave.com"].include? event.modified_by.id
+        new_sentence = true
+        event.blip.content.length.times do |index|
+          range = index..index+1
+          char = blip.content[index, 1]
+          if char =~ /[A-Z]/ && !new_sentence
+            blip.set_text_in_range(range, char.downcase)
+          elsif char =~ /[a-z]/ && new_sentence
+            blip.set_text_in_range(range, char.upcase)
+          elsif char == "!"
+            if new_sentence
+              blip.delete_range(range)
+            else
+              blip.set_text_in_range(range, ".")
             end
-            new_sentence = (char =~ /\.!?/ || (char =~ /\s/ && new_sentence))
           end
+          new_sentence = (char =~ /\.!?/ || (char =~ /\s/ && new_sentence))
         end
       end
     end
