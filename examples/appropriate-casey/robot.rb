fc2d3713e1004aa25e581c828da951399ad4e652
Bil Bas <bil.bagpuss@gmail.com>
2009-12-09 16:50:12 +0000

    MODIFIED: Slightly improved handling of ! (and added handling of ?) for appropriate-casey

--- examples/appropriate-casey/robot.rb
+++ examples/appropriate-casey/robot.rb
@@ -7,8 +7,9 @@ module AppropriateCasey
     def blip_submitted(event, context)
       blip = event.blip # Blip that was submitted.
       text = blip.content
-      text.gsub!(/(\s*)([^!\.]+)/) { $1 + $2.capitalize } # Capitalize sentances.
-      text.gsub!(/!+/, '.') # Calm down exclaimations.
+      text.gsub!(/(\s*)([^!\.\?]+)/) { $1 + $2.capitalize } # Capitalize sentances.
+      text.gsub!(/\.*!+/, '.') # Calm down exclaimations.
+      text.gsub!(/\?+/, '?') # Calm down question marks.
       blip.set_text(text) if text != blip.content
     end
   end
